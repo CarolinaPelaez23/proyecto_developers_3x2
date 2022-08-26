c59c1910c33de2cd71be2a963b94c0f53f8eb98c
Natalia <natalia.duitama1@gmail.com>
2022-08-25 21:48:02 -0500

    Clase Transaction

--- src/main/java/developers3x2/proyecto/entidad/Transaction.java
+++ src/main/java/developers3x2/proyecto/entidad/Transaction.java
@@ -1,4 +1,78 @@
 package developers3x2.proyecto.entidad;
 
+import java.util.Date;
 public class Transaction {
+    private long id;
+    private String concept;
+    private float amount;
+    private user User;
+    private enterprise Enterprise;
+    private Date createdAt;
+    private Date  updateAt;
+
+    public Transaction(long id, String concept, float amount, user user, enterprise enterprise, Date createdAt, Date updateAt) {
+        this.id = id;
+        this.concept = concept;
+        this.amount = amount;
+        User = user;
+        Enterprise = enterprise;
+        this.createdAt = createdAt;
+        this.updateAt = updateAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getConcept() {
+        return concept;
+    }
+
+    public void setConcept(String concept) {
+        this.concept = concept;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public user getUser() {
+        return User;
+    }
+
+    public void setUser(user user) {
+        User = user;
+    }
+
+    public enterprise getEnterprise() {
+        return Enterprise;
+    }
+
+    public void setEnterprise(enterprise enterprise) {
+        Enterprise = enterprise;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
 }
