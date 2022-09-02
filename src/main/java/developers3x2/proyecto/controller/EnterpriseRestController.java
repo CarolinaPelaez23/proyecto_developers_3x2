1de501b127f01faeb63b8d8b151a6bfe0f8ee5d7
Jefferson Ramirez <jefferson.ramirezgar@gmail.com>
2022-09-01 19:35:31 -0500

    Creacion de EnterpriseRestController

---
+++ src/main/java/developers3x2/proyecto/controller/EnterpriseRestController.java
@@ -0,0 +1,49 @@
+package developers3x2.proyecto.controller;
+
+import developers3x2.proyecto.entidad.Enterprise;
+import developers3x2.proyecto.service.IEnterpiseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+
+public class EnterpriseRestController {
+
+    @Autowired
+
+    private IEnterpiseService enterpriseService;
+
+    @GetMapping("/enterprise/{id}")
+    public Enterprise findById(@PathVariable int id){
+        return enterpriseService.findById(id);
+    }
+
+    @GetMapping("/enterprise")
+    public List<Enterprise> findAll(){
+        return enterpriseService.findAll();
+    }
+
+    @PostMapping("/enterprise")
+    public Enterprise createEnterprise(@RequestBody Enterprise enterprise){
+        return enterpriseService.createEnterprise(enterprise);
+    }
+
+    @PutMapping("/enterprise/{id}")
+    public Enterprise updateEnterprise(@PathVariable int id, @RequestBody Enterprise enterprise) {
+        return enterpriseService.updateEnterprise(id, enterprise);
+    }
+
+    @DeleteMapping("/enterprise/{id}")
+    public void deleteEnterprise(@PathVariable int id){
+        enterpriseService.deleteEnterprise(id);
+        //Buscando y borrando
+    }
+
+
+
+
+
+}
