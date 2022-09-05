44960b7e0cc56dadaec316db9a8682ced0b2bd29
proxer1982 <juanc.zorro@gmail.com>
2022-09-05 15:45:06 -0500

    Agregado Rest controller de usuario por parte de Leon

--- src/main/java/developers3x2/proyecto/service/UsuarioService.java
+++ src/main/java/developers3x2/proyecto/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package developers3x2.proyecto.service;
 
+import developers3x2.proyecto.entidad.RoleName;
 import developers3x2.proyecto.entidad.Usuario;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +13,18 @@ public class UsuarioService implements IUsuarioService{
 
     @Override
     public Usuario findById(int id) {
-        return null;
+        Usuario newUsuario = new Usuario(1, "mail@usuairo.com", RoleName.Admin, null, true);
+        return newUsuario;
     }
 
     @Override
     public List<Usuario> findAll() {
-
-        return null;
+        List<Usuario> usuarios = new ArrayList<Usuario>();
+        Usuario newUsuario1 = new Usuario(1, "mail@usuairo.com", RoleName.Admin, null, true);
+        Usuario newUsuario2 = new Usuario(1, "mail@usuairo.com", RoleName.Admin, null, true);
+        usuarios.add(newUsuario1);
+        usuarios.add(newUsuario2);
+        return usuarios;
     }
 
     @Override
